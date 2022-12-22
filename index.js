d45728effd3facba4145780a116fb2dbe866383e
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-12-22 23:26:25 +0700

    Add content to html files

---
+++ index.js
@@ -0,0 +1,28 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+
+http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = ".";
+
+    if (q.pathname === "" || q.pathname === "/") {
+        filename += "/index.html"
+    } else if (q.pathname === "/about") {
+        filename += "/about.html"
+    } else if (q.pathname === "/contact-me") {
+        filename += "/contact-me.html"
+    } else {
+        filename += "/404.html"
+    }
+
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
